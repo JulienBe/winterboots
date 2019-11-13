16644d16804b55c4cf66b6f1a470aab78478d402
JulienBe <bertozzijulien@gmail.com>
2019-11-13 21:55:41 +0100

    initial commit of the app generated by spring initializr

---
+++ src/test/java/be/julien/winterboots/WinterbootsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.julien.winterboots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterbootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
